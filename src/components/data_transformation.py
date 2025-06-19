6dc9000ac63d4ef8edef205b764e684765457a5d
Pasuwat Sirasareerot <pasuwat.sira@gmail.com>
2025-06-19 18:14:23 +0700

    app.py, predict pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,7 +28,7 @@ class DataTrasformation:
             categorical_columns = [
                 "gender",
                 "race_ethnicity",
-                "parental_level_of_education",
+                "parentual_level_of_education",
                 "lunch",
                 "test_preparation_course"
             ]
